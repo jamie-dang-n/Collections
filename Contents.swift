e7fb11c9194cb90b72dc5e07d33cd752188bc26a
Anika <pikainka@gmail.com>
2019-07-09 13:13:22 -0700

    First commit

---
+++ Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+//var arrayOfStrings = ["Yeehaw!", "Put as many strings as you want in here!"]
+//var arrayOfIntegers = [1, 2, 3, 4]
+//
+//print the number 4 that is stored at location 3
+//print(arrayOfIntegers[3])
+
+//I stored 4 strings which are names of people
+
+//Alara has an index of 0
+//Ruhbver has an index of 1
+//Yaeeeeeeeel has an index of 2
+//Eggan has an index of 3
+//4 names/elements, but we count from 0 for collections
+var buddiesOfMine = ["Alora AlbArt", "Ruhbver Zuhhhbgr", "Yaeeeeeel Ekululule", "Eggan Waahaklker"]
+
+//River is printed
+print(buddiesOfMine[1])
+
+//Prints Ethan
+print(buddiesOfMine[3])
+
+//index out of range error, we can only go up to 3 (bcs even though its 4 #, it starts from 0
+//print(buddiesOfMine[10])
+
+//No go! Can't have diff data types in arrays
+//var mixedDataTypes = [1, "Michelle Obama", 10]
+
